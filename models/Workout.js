89185fb00f6bbe9777ddf2d2e0f1f564913d7ec4
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-10-09 20:59:36 -0400

    Update workout schema - change exercises field, add new fields

--- models/Workout.js
+++ models/Workout.js
@@ -5,15 +5,59 @@ const WorkoutSchema = new Schema({
   day: {
     type: Date,
     default: Date.now },
-    
+
+  // exercises: [
+  //   {
+  //     type: Schema.Types.ObjectId, // type is the id given to each exercise
+  //     ref: "Exercise" // relates the Exercise collection to the Workout collection
+  //   }
+  // ],
+
   exercises: [
     {
-      type: Schema.Types.ObjectId, // type is the id given to each exercise
-      ref: "Exercise" // relates the Exercise collection to the Workout collection
+      name: {
+        type: String,
+        trim: true,
+      },
+
+      duration: {
+        type: Number,
+        required: "Duration is required."
+      }
     }
-  ]
+  ],
+
+  totalDistance: {
+    type: Number,
+    default: 0
+  },
+
+  totalDuration: {
+    type: Number,
+    default: 0
+  },
+
+  totalWeight: {
+    type: Number,
+    default: 0
+  },
+
+  totalSets: {
+    type: Number,
+    default: 0
+  },
+
+  totalReps: {
+    type: Number,
+    default: 0
+  }
+
 });
 
-const Workout = mongoos.model("Workout", WorkoutSchema);
+// UserSchema.methods.setTotalDistance = function() {
+
+// }
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout;
